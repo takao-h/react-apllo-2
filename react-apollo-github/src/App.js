56e968068c1aa69c29fea35198d137802f6e9daa
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-06-18 01:45:38 +0900

    fix App.js

--- react-apollo-github/src/App.js
+++ react-apollo-github/src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5, isFork: false) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
   }
-}
+`;
+
+const App = () => (
+  <Query query={query}> 
+      {({ loading, data }) => {
+      if (loading) return <p>Loading...</p>;
+
+      const repositories = data.organization.repositories.nodes;
+
+      return (
+        <ul>
+          {repositories.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.url}>{repo.name}</a>
+              <button>{repo.stargazers.totalCount} Star</button>
+            </li>
+          ))}
+        </ul>
+      );
+    }}
+  </Query>
+)
 
 export default App;
